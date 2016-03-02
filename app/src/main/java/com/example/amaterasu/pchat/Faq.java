9cbe12fc5aec51a2f81a916138c4de3c7e4d84de
Shailendra <goforsunny1993@gmail.com>
2016-03-03 04:51:55 +0530

    merged with yogi, about help content

---
+++ app/src/main/java/com/example/amaterasu/pchat/Faq.java
@@ -0,0 +1,10 @@
+package com.example.amaterasu.pchat;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by yogesh on 3/3/16.
+ */
+public class Faq extends AppCompatActivity{
+
+}
