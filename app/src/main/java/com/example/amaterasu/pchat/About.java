ac18d7f78b5caf2aefb5853df912f37fa104493d
yogesh-verma <yogesh.231295@gmail.com>
2016-03-03 04:08:32 +0530

    added help layout

---
+++ app/src/main/java/com/example/amaterasu/pchat/About.java
@@ -0,0 +1,16 @@
+package com.example.amaterasu.pchat;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by yogesh on 3/3/16.
+ */
+public class About extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+    }
+}
---
+++ app/src/main/java/com/example/amaterasu/pchat/ContactUs.java
@@ -0,0 +1,15 @@
+package com.example.amaterasu.pchat;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by yogesh on 3/3/16.
+ */
+public class ContactUs extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.contact_us);
+    }
+}
